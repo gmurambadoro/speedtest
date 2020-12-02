304c398420a7315445197b37318986756308124d
Gavin Murambadoro <developer.gavin@gmail.com>
2020-12-02 12:18:38 +0200

    First commit with entity and api support

---
+++ src/Entity/Server.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ServerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ServerRepository::class)
+ * @ApiResource()
+ */
+class Server
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private string $sponsor;
+
+    /**
+     * @ORM\Column(type="integer", unique=true)
+     */
+    private int $serverId = 0;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $serverName;
+
+    public function __toString()
+    {
+        return (string)$this->getSponsor();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSponsor(): ?string
+    {
+        return $this->sponsor;
+    }
+
+    public function setSponsor(string $sponsor): self
+    {
+        $this->sponsor = $sponsor;
+
+        return $this;
+    }
+
+    public function getServerId(): ?int
+    {
+        return $this->serverId;
+    }
+
+    public function setServerId(int $serverId): self
+    {
+        $this->serverId = $serverId;
+
+        return $this;
+    }
+
+    public function getServerName(): ?string
+    {
+        return $this->serverName;
+    }
+
+    public function setServerName(string $serverName): self
+    {
+        $this->serverName = $serverName;
+
+        return $this;
+    }
+}
---
+++ src/Entity/SpeedTest.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SpeedTestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SpeedTestRepository::class)
+ */
+class SpeedTest
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Server::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $server;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $timestamp;
+
+    /**
+     * @ORM\Column(type="decimal", precision=15, scale=15)
+     */
+    private $distance;
+
+    /**
+     * @ORM\Column(type="decimal", precision=15, scale=15)
+     */
+    private $ping;
+
+    /**
+     * @ORM\Column(type="decimal", precision=15, scale=15)
+     */
+    private $downloadSpeed;
+
+    /**
+     * @ORM\Column(type="decimal", precision=15, scale=15)
+     */
+    private $uploadSpeed;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $share;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $ipAddress;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getServer(): ?Server
+    {
+        return $this->server;
+    }
+
+    public function setServer(?Server $server): self
+    {
+        $this->server = $server;
+
+        return $this;
+    }
+
+    public function getTimestamp(): ?\DateTimeImmutable
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(\DateTimeImmutable $timestamp): self
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    public function getDistance(): ?string
+    {
+        return $this->distance;
+    }
+
+    public function setDistance(string $distance): self
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+
+    public function getPing(): ?string
+    {
+        return $this->ping;
+    }
+
+    public function setPing(string $ping): self
+    {
+        $this->ping = $ping;
+
+        return $this;
+    }
+
+    public function getDownloadSpeed(): ?string
+    {
+        return $this->downloadSpeed;
+    }
+
+    public function setDownloadSpeed(string $downloadSpeed): self
+    {
+        $this->downloadSpeed = $downloadSpeed;
+
+        return $this;
+    }
+
+    public function getUploadSpeed(): ?string
+    {
+        return $this->uploadSpeed;
+    }
+
+    public function setUploadSpeed(string $uploadSpeed): self
+    {
+        $this->uploadSpeed = $uploadSpeed;
+
+        return $this;
+    }
+
+    public function getShare(): ?string
+    {
+        return $this->share;
+    }
+
+    public function setShare(?string $share): self
+    {
+        $this->share = $share;
+
+        return $this;
+    }
+
+    public function getIpAddress(): ?string
+    {
+        return $this->ipAddress;
+    }
+
+    public function setIpAddress(string $ipAddress): self
+    {
+        $this->ipAddress = $ipAddress;
+
+        return $this;
+    }
+}
