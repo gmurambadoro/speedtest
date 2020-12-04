870eb9b02b2b304fd1e5a3291f948b89d64f58f6
Gavin Murambadoro <developer.gavin@gmail.com>
2020-12-04 14:26:58 +0200

    Rename API endpoints

--- src/Entity/ServiceProvider.php
+++ src/Entity/ServiceProvider.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ApiResource(
  *     collectionOperations={"get"},
  *     itemOperations={"get"},
- *     shortName="Service-Provider",
+ *     shortName="Internet-Service-Provider",
  * )
  */
 class ServiceProvider
--- src/Entity/SpeedTest.php
+++ src/Entity/SpeedTest.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ApiResource(
  *     collectionOperations={"get"},
  *     itemOperations={"get"},
- *     shortName="Speed-Test",
+ *     shortName="Speed",
  *     order={"timestamp"="DESC"}
  * )
  */
